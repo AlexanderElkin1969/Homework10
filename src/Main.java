aaa116da24f074cb297ce7dbb3ea23459e0a1ec1
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-01-24 20:28:35 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName+" "+firstName+" "+middleName;
+        System.out.println("Задача 1");
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println("Задача 2");
+        System.out.println("Задача 3");
     }
 }
