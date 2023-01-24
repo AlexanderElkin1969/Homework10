b48d283389147f05efe4291ff3cf0cd07b03ffd6
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-01-24 20:44:26 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ public class Main {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
-        String fullName = lastName+" "+firstName+" "+middleName;
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Задача 1");
         System.out.println("ФИО сотрудника — " + fullName);
         System.out.println("Задача 2");
-        fullName = fullName.toUpperCase();
-        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
         System.out.println("Задача 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
